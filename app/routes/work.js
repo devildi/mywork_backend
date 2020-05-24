3e63b1222b3d9fb92f9d92a1aa81ec489ebaee8c
DevilDI <DevilDI@appledeMBP-3.lan>
2020-05-24 20:35:05 +0800

    2020-5-24

--- app/routes/work.js
+++ app/routes/work.js
@@ -1,6 +1,6 @@
 const jwt = require('koa-jwt');
 const Router = require('koa-router');
-const router = new Router({ prefix: '/work' });
+const router = new Router({ prefix: '/api/work' });
 
 const { secret } = require('../config');
 const auth = jwt({ secret });
