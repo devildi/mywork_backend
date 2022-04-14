32469280c1b00141f546e14142c38d1cfa02a6fb
wu <wudi>
2022-04-14 11:13:17 +0800

    2022-4-14

--- GRPC/microService/article.py
+++ GRPC/microService/article.py
@@ -9,43 +9,51 @@ class ArticleSpider(scrapy.Spider):
         sections = response.xpath('//*[@id="js_content"]/section/section/section/section')
         for quote in sections:
             title = quote.xpath('section//strong/text()').get()
-            para = quote.css('p::text')
+            para = quote.xpath('p')
             img = quote.css('img').xpath('@data-src').get()
             text = ''
             if title:
                 arr.append(title)
             if para:
                 for p in para:
-                    text = text + p.get()
+                    if p.xpath('./text()').get():
+                        text = text + p.xpath('./text()').get()
+                    spans = p.xpath('span')
+                    if spans:
+                        for s in spans:
+                            text = text + s.xpath('./text()').get()
             if img:
                 arr.append(img)
             if text != '':
                 arr.append(text)
-        section1 = response.xpath('//*[@id="js_content"]/section/section')
+        section1 = response.xpath('//*[@id="js_content"]/section/section|//*[@id="js_content"]/section/p')
         for s in section1:
-            para = s.css('p::text')
-            span = s.css('span::text')
+            para = s.css('p')
             strong = s.css('strong::text')
-            img = s.css('img').xpath('@data-src').get()
+            img = s.css('img').xpath('@data-src')
             end = s.css('section::text')
             text = ''
             if para:
                 for p in para:
-                    text = text + p.get()
-            if span:
-                for w in span:
-                    print(w.get())
-                    text = text + w.get()
+                    for pp in p.css('p::text'):
+                        text = text + pp.get()
+                    spans = p.css('span')
+                    if spans:
+                        for span in spans:
+                            spanArray = span.xpath('./text()')
+                            for span1 in spanArray:
+                                text = text + span1.get()
             if strong:
                 for r in strong:
                     text = text + r.get()
+            if text != '':
+                arr1.append(text)
             if img:
-                arr1.append(img)
+                for i in img:
+                    arr1.append(i.get())
             if end:
                 for e in end:
                     arr1.append(e.get())
-            if text != '':
-                arr1.append(text)
         if(len(arr) > len(arr1)):
             return {'result': arr}
         else:
