0ca1b7706a3afe98b0b8fa723e756a151cba8807
wu <wudi>
2022-11-18 13:55:53 +0800

    202-11-18

--- app/index.js
+++ app/index.js
@@ -8,15 +8,12 @@ const logger = require('koa-logger')
 const path = require('path')
 const routing = require('./routes')
 const { port } = require('./config')
-//const IO = require('koa-socket')
 const db = 'mongodb://woody:41538bc6dd@localhost/davinci'
 const app = new Koa()
 
 const server = require('http').createServer(app.callback())
 const { Server } = require("socket.io");
 const io = new Server(server);
-
-//const io = new IO()
 
 app.use(logger())
 mongoose.connect(db,{useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
@@ -35,6 +32,7 @@ app.use(koaBody({
     keepExtensions: true,
   },
 }))
+
 app.use(async(ctx, next) => {
   if (ctx.path == '/api/users/newClient') {
     // console.log('client')
@@ -46,21 +44,12 @@ app.use(async(ctx, next) => {
 
 app.use(parameter(app))
 routing(app)
-//io.attach(app)
-// app._io.on( 'connection', socket => {
-//   console.log('建立连接了');
-//   let roomid=url.parse(socket.request.url,true).query.roomid;   /*获取房间号/ 获取桌号需要引入url模块*/
-//   socket.join(roomid);  /*加入房间/加入分组*/
-//   socket.on('message',function(data){
-//   console.log(data);
-//     //socket.emit('serverEmit','我接收到消息了');  /*发给指定用户*/
-//     //app._io.emit('serverEmit','我接收到消息了');  /*广播*/
-//     //app._io.to(roomid).emit('serverEmit','我接收到消息了'); 发送给房间内的所有人
-//     socket.broadcast.to(roomid).emit('serverEmit','我接收到消息了');/*发送给除了自己以外房间内的所有人*/
-//   })
-// })
 io.on('connection', (socket) => {
   console.log('socket已连接！',socket.id)
+  socket.on('chat message', (msg) => {
+    //socket.broadcast.emit('data', msg);
+    io.emit('data', msg);
+  });
 })
 
 server.listen(port, () => console.log(`程序启动在 ${port} 端口`))
